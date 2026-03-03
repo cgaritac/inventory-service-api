036dadf4e6ee3518ded9c0c5286ce79535324a24
Carlos Garita Campos <cgaritac@gmail.com>
2026-03-02 23:50:20 -0600

    Feat: Add permissions

---
+++ apps/users/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+from apps.users.models import user
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role in [
+            User.Role.OWNER,
+            User.Role.ADMIN
+        ]
+    
+class IsManagerOrAbove(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role in [
+            User.Role.OWNER,
+            User.Role.ADMIN,
+            User.Role.MANAGER
+        ]
