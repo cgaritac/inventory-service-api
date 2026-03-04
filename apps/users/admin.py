6fb03cf1b7eaf3524597f622e4ea4515db63e549
Carlos Garita Campos <cgaritac@gmail.com>
2026-03-03 23:38:23 -0600

    Feat: Update admin form

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('role', 'company')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role', 'company')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('role', 'company')}),
+    )
