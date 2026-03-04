7b2df85efa0f73554b9d5ef6207bab406ec9d6e6
Carlos Garita Campos <cgaritac@gmail.com>
2026-03-03 23:19:49 -0600

    Feat: Update softdelete behavior

--- core/models.py
+++ core/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class SoftDeleteQuerySet(models.QuerySet):
     def delete(self):
-        return super().update(is_deleted=True)
+        return super().update(
+            is_deleted=True,
+            deleted_at=timezone.now()
+        )
     
     def hard_delete(self):
         return super().delete()
@@ -20,17 +24,16 @@ class SoftDeleteManager(models.Manager):
 class BaseModel(models.Model):
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=False, null=True, blank=True)
-    deleted_at = models.DateTimeField(auto_now=False, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     objects = SoftDeleteManager()
     all_objects = models.Manager()
 
     def delete(self, *args, **kwargs):
         self.is_deleted = True
-        self.updated_at = timezone.now()
         self.deleted_at = timezone.now()
-        self.save()
+        self.save(update_fields=['is_deleted', 'deleted_at'])
 
     class Meta:
         abstract = True
