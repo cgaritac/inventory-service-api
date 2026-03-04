1addd2ffd4c2d0f1dbf096bc32316b82c6f6a570
Carlos Garita Campos <cgaritac@gmail.com>
2026-03-03 23:04:41 -0600

    Feat: Add softdelete model

---
+++ core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(is_deleted=True)
+    
+    def hard_delete(self):
+        return super().delete()
+    
+    def alive(self):
+        return self.filter(is_deleted=False)
+    
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).filter(is_deleted=False)
+
+class BaseModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.save()
+
+    class Meta:
+        abstract = True
